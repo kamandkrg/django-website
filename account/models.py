c963d8024eeb6929657259b8d3901fe0fc2541d0
kamandkrg <kamandkargar@gmail.com>
2023-01-19 19:23:32 +0330

    start user manager for account

--- account/models.py
+++ account/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from phonenumber_field.modelfields import PhoneNumberField
 from django.db import models
+from account.manager import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
