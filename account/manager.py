ceb244a071c27f09c52dffd1219419ebb5b29f13
kamandkrg <kamandkargar@gmail.com>
2023-01-21 22:45:33 +0330

    make manager for account

--- account/manager.py
+++ account/manager.py
@@ -2,6 +2,28 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    pass
+    use_in_migrations = True
+
+    def _create_user(self, username, email, phone, password, **extra_fields):
+        if not username or not email or not phone:
+            raise ValueError('username, email and phone must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, phone=phone)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_user(self, username, email, phone, password, **extra_field):
+        extra_field.setdefault('is_superuser', False)
+        extra_field.setdefault('is_admin', False)
+        return self._create_user(username, email, phone, password, **extra_field)
+
+    def create_superuser(self, username, email, phone, password, **extra_field):
+        extra_field.setdefault('is_superuser', True)
+        if extra_field.get('is_superuser') is not True:
+            raise ValueError('is_superuser must be True')
+        return self._create_user(username, email, phone, password, **extra_field)
+
+
 
 
