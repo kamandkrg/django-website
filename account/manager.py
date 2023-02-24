fb2029bf35c51fef71b8a223dd259381c93ff3a6
kamandkrg <kamandkargar@gmail.com>
2023-02-24 15:30:46 +0330

    make custome manager for user

--- account/manager.py
+++ account/manager.py
@@ -1,3 +1,4 @@
+import phonenumbers
 from django.contrib.auth.models import BaseUserManager
 
 
@@ -8,19 +9,20 @@ class UserManager(BaseUserManager):
         if not username or not email or not phone:
             raise ValueError('username, email and phone must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, username=username, phone=phone)
+        user = self.model(email=email, username=username, phone=phone, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
     def create_user(self, username, email, phone, password, **extra_field):
         extra_field.setdefault('is_superuser', False)
-        extra_field.setdefault('is_admin', False)
+        extra_field.setdefault('is_staff', False)
         return self._create_user(username, email, phone, password, **extra_field)
 
     def create_superuser(self, username, email, phone, password, **extra_field):
         extra_field.setdefault('is_superuser', True)
-        if extra_field.get('is_superuser') is not True:
+        extra_field.setdefault('is_staff', True)
+        if (extra_field.get('is_superuser') and extra_field.get('is_staff')) is not True:
             raise ValueError('is_superuser must be True')
         return self._create_user(username, email, phone, password, **extra_field)
 
--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,52 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+class UsersTest(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user('kamand', 'kamandkargar@gmail.com', '+989123456789', '1234')
+        self.assertEqual(user.email, 'kamandkargar@gmail.com')
+        self.assertEqual(user.username, 'kamand')
+        self.assertEqual(user.phone, '+989123456789')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+        try:
+            self.assertIsNotNone(user.username)
+            self.assertIsNotNone(user.email)
+            self.assertIsNotNone(user.phone)
+        except AttributeError:
+            pass
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email='')
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email='kamandkargar@gmail.com', username='')
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email='kamandkargar@gmail.com', username='kamand', phone='')
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email='', username='kamandkrg', phone='+989123456789', password='12345')
+        with self.assertRaises(ValueError):
+            User.objects.create_user(
+                email='kamandkrg@gmail.com', username='kamandkrg', phone='+7894523025487523', password='12345'
+            )
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            email="super@user.com", username='kamand', phone="+989123456789", password='1234'
+        )
+        self.assertEqual(admin_user.email, "super@user.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+        try:
+            self.assertIsNotNone(admin_user.username)
+            self.assertIsNotNone(admin_user.email)
+            self.assertIsNotNone(admin_user.phone)
+        except AttributeError:
+            pass
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email="super@user.com", username='kamand', phone='+989123456789', password="1234", is_superuser=False)
