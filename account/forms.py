37596e918c1cef22c289655ced8cafa5f4592bf5
kamandkrg <kamandkargar@gmail.com>
2023-03-10 21:22:16 +0330

    add forms for panel admin

--- account/forms.py
+++ account/forms.py
@@ -3,9 +3,12 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 User = get_user_model()
+
+
 class NewUserForm(forms.ModelForm):
     password1 = forms.CharField(label='password')
     password2 = forms.CharField(label='password confirmation')
+
     class Meta:
         mode = User
         fields = ('email', 'username', 'phone')
@@ -16,6 +19,7 @@ class NewUserForm(forms.ModelForm):
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError('password don\'t match')
         return password2
+
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data['password2'])
@@ -23,8 +27,10 @@ class NewUserForm(forms.ModelForm):
             user.save()
         return user
 
+
 class UserChangeForm(forms.ModelForm):
     password = ReadOnlyPasswordHashField()
+
     class Meta:
         model = User
         fields = ('username', 'phone', 'email', 'first_name', 'last_name', 'age', 'sex', 'national_code', 'job', 'birthday', 'is_active', 'is_seller', 'is_staff', 'is_superuser')
